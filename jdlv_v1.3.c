728a6ac9edec12bb64dcdab31b40f68739497fe1
Matias Gonzalez Perez <161659100+matogperez@users.noreply.github.com>
2024-10-24 17:13:46 -0600

    Add files via upload

---
+++ jdlv_v1.3.c
@@ -0,0 +1,259 @@
+//Juego de la vida. Segundo intento, simplificando la cuenta de los vecinos con fors en lugar de hacerlo directo
+
+#include<stdio.h> //libreria standard input output
+#include<stdlib.h> //librería standard en donde están la función rand 
+#include<time.h> //Libreria para poder llamar la función de tiempo
+#include<stdbool.h> //Libreria para que acepte términos booleanos
+#include<unistd.h>
+
+
+int i,j,conteo,x,k,l; //creamos 5 espacios en la memoria para enteros
+
+const int N = 30;  //creamos un espacio para un entero constante que será la base de la matriz
+
+
+
+
+//Declaramos la función principal
+int main (void){
+
+int matriz[N][N]; //Declaramos una matriz de nxn. En ste caso le damos explicitamente con número la matriz
+
+int mconteo[N][N]; //Declaramos matriz para almacenar suma de vecinos de matriz original
+
+	
+	//esta función genera una semilla para generar un número aleatorio. Dentro del primer paréntesis se pone "time" para indicar que el tiempo es lo que determinará la semilla.
+	srand(time(NULL));	
+	
+	//el primer for con la instrucción de ejecutarse de 0 a n. El segundo for hace lo mismo.
+	//Esto genera una matriz de dimensión 2 
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+		
+			//Se le asigna un valor a la variable x que va de 0 a 99
+			x=rand()%100;
+			
+			//Condicional para que en alrededor de un quinto de las veces haga 1
+			if(x<20){
+				matriz[i][j]=1;	
+			
+			//Condición para hacer 0 el resto de la matriz
+			}
+			else{
+				matriz[i][j]=0;
+		
+			}	
+		}
+	
+	}
+	
+	
+	//Fors para imprimir la matriz
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			printf ("%i ", matriz[i][j]);
+		}
+	printf("\n");
+	}
+	
+	
+	
+	int m = 3;
+	//int ph = 0;
+	while(true){
+	//ph++;
+	//fors para crear la matriz de conteo
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+		
+		//Condición para borde izquierdo, suma las i-1 con los bordes derechos
+			if((i==0)&&(j!=N-1)&&(j!=0)){
+				for(l=0;l<3;l++){
+					for (k=0;k<2;k++){
+						
+						mconteo[i][j] += matriz[i+k][j-1+l];
+					}
+				
+					mconteo[i][j] += matriz[N-1][j-1+l];
+				
+				}
+			
+			}
+			
+			//Condición borde derecho, suma las i+1 con el borde izquierdo
+			else if((i==(N-1))&&((j!=N-1)&&(j!=0))){
+				for(l=0;l<3;l++){
+					for (k=0;k<2;k++){
+						
+						mconteo[i][j] += matriz[i-1+k][j-1+l];
+					}
+				
+					mconteo[i][j] += matriz[0][j-1+l];
+				
+				}
+			
+			mconteo[i][j] -= matriz[i][j];
+			
+			}
+			
+			//Condición borde superior, suma j-1 con borde inferior
+			else if((j==0)&&((i!=N-1)&&(i!=0))){
+			
+				for(l=0;l<3;l++){
+					for(k=0;k<2;k++){
+						
+						mconteo[i][j] += matriz[i-1+l][j+k];
+					}
+				
+					mconteo[i][j] += matriz[i-1+l][N-1];
+				
+				}
+				
+					
+				mconteo[i][j] -= matriz[i][j];
+			}
+			
+			//Condición borde inferior, suma j+1 con el borde superior
+			else if(j==(N-1)&&((i!=0)&&(i!=N-1))){
+			
+				for(l=0;l<3;l++){
+					for(k=0;k<2;k++){
+						
+						mconteo[i][j] += matriz[i-1+l][j-1+k];
+					}
+					
+					mconteo[i][j] += matriz[i-1+l][0];
+				
+				}
+				
+				mconteo[i][j] -= matriz[i][j];
+
+			}
+			
+			//Condición esquina superior izquierda, suma i-1 con borde izquierdo y j-1 con borde inferior
+			else if((i==0)&&(j==0)){
+			
+				for(l=0;l<2;l++){
+					for(k=0;k<2;k++){
+					
+						mconteo[i][j] += matriz[i+k][j+l];
+					
+					}
+				
+					mconteo[i][j] += matriz[N-1][j+l];
+					mconteo[i][j] += matriz[i+l][N-1];
+				
+				}
+				
+				mconteo[i][j] += matriz[N-1][N-1];
+				mconteo[i][j] -= matriz[i][j];
+			}
+			
+			//Condición esquina superior derecha, suma i+1 con borde izquierdo y j-1 con borde inferior
+			else if((i==N-1)&&(j==0)){
+			
+				for(l=0;l<2;l++){
+					for(k=0;k<2;k++){
+						
+						mconteo[i][j] += matriz[i-1+k][j+l];
+					}
+					mconteo[i][j] += matriz[0][j+l];
+					mconteo[i][j] += matriz[i-1+l][N-1];
+				}
+				mconteo[i][j] += matriz[0][N-1];
+				mconteo[i][j] -= matriz[i][j];
+				
+				
+			}
+			
+			//Condición esquina inferior izquierda, suma i-1 con borde derecho y j+1 con borde superior
+			else if((i==0)&&(j==N-1)){
+			
+				for(l=0;l<2;l++){
+					for(k=0;k<2;k++){
+					
+						mconteo[i][j] += matriz[i+k][j-1+l];
+					
+					}
+					
+					
+					mconteo[i][j] += matriz[N-1][j-1+l];
+					mconteo[i][j] += matriz[i+l][0];
+				}
+				
+				mconteo[i][j] += matriz[N-1][0];
+				mconteo[i][j] -= matriz[i][j];
+				
+			}
+			
+		//Condición esquina inferior derecha, suma i+1 con borde izquierdo y j+1 con borde superior
+			else if((i==N-1)&&(j==N-1)){
+			
+				for(l=0;l<2;l++){
+					for(k=0;k>2;k++){
+					
+						mconteo[i][j] += matriz[i-1+k][j-1+l];
+					
+					}
+					
+					mconteo[i][j]+=matriz[i-1+l][0];
+					mconteo[i][j]+=matriz[0][j-1+l];
+					
+				}
+				
+				mconteo[i][j]+=	matriz[0][0];
+				mconteo[i][j]-= matriz[i][j];
+			}
+			
+			//Condición para el resto de la matriz para sumar valor de vecions
+			else{
+				for(l=0;l<m;l++){
+					for(k=0;k<m;k++){
+						
+						mconteo[i][j] += matriz[i-1+k][j-1+l];
+					}
+				}
+				mconteo[i][j] -= matriz[i][j];
+				
+			}
+		}
+	}
+	
+	//reglas del juego
+	
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			if((matriz[i][j]==0)&&(mconteo[i][j]==3)){
+				
+				matriz[i][j] = 1;
+			}
+				
+			else if((matriz[i][j]==1)&&((mconteo[i][j]>3)||(mconteo[i][j]<2))){
+			
+				matriz[i][j]=0;
+			}
+			
+			else{
+			
+			matriz[i][j]=matriz[i][j];
+			
+			}
+			mconteo[i][j]=0;
+		}
+	}
+	//printf("\n");
+	//int ph = 0;
+	//scanf("Continuar: %i", &ph);
+	printf("\e[1;1H\e[2J");
+	
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			printf("%i ", matriz[i][j]);
+		
+		}
+		printf("\n");
+	}
+	sleep(1);
+	}
+return 0;
+}	
